ab93a89d6844b9fb32217a3b44844bfbebb2b515
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-06 20:33:59 +0500

    added icon website link in html head and icons link in js variables

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let toastBox = document.querySelector("#toastBox");
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Submitted';
+let errorMsg =   '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
-function showToast(){
+function showToast(msg){
     let toast = document.createElement("div");
     toast.classList.add("toast");
-    toast.innerHTML = "success";
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
 }
